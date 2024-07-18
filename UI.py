aff4e9e3d6172f6a344c3b43a33b2b1f92d91517
ManishGriffith <140369182+ManishGriffith@users.noreply.github.com>
2024-07-18 16:03:25 +1000

    Update UI.py

--- UI.py
+++ UI.py
@@ -30,7 +30,7 @@ def read_csv(csv):
             st.dataframe(filtered_data[selected_columns])
 
         # create a separate button to display accident type
-        display_data_for_accident_type(filtered_data, selected_year, accident_type)
+        display_data_for_accident_type(filtered_data)
         # create a button to display accidents per hour
         display_accidents_per_hour(filtered_data, selected_year)
         # create a button for alcohol impacts
